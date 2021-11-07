768615e45e0fcae85067bed1261fdabc5c3357a3
teerakitchai <46394384+teerakitchai@users.noreply.github.com>
2021-11-07 20:27:46 +0700

    add project layout

---
+++ layout/lib/pages/detail.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({ Key? key }) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Detail")),
+      body: ListView(
+        children: [
+          Text("หน้า Detail ของบทความที่ต้องการอธิบาย")
+        ],
+      ),
+      
+    );
+  }
+}
